100ce5cca22597e5e46847060d272912cb10da9b
Fariz <frzrzky@gmail.com>
2021-10-11 00:54:53 +0700

    migration

---
+++ database/migrations/2021_10_10_130800_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->char('id',26)->primary();
+            $table->string('page_id');
+            $table->string('title');
+            $table->timestamps();
+
+            $table->foreign('page_id')
+                ->references('id')
+                ->on('pages')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
--- database/seeders/BaseTableSeeder.php
+++ database/seeders/BaseTableSeeder.php
@@ -3,9 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Http\Request;
 
 use App\Models\Role;
 use App\Models\User;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 class BaseTableSeeder extends Seeder
 {
@@ -42,5 +46,89 @@ class BaseTableSeeder extends Seeder
 
         $admin->assignRole('admin');
         $cashier->assignRole('cashier');
+
+        $request = new Request();
+
+        $page_controller = new PageController();
+        $category_controller = new CategoryController();
+        $product_controller = new ProductController();
+
+        // Page restoran
+        $request->replace([
+            'title' => 'Restoran',
+        ]);
+        $restoran = $page_controller->create($request);
+
+        // Restoran Category
+        $request->replace([
+            'page_id' => $restoran->id,
+            'title' => 'Makanan',
+        ]);
+        $cat_resto_1 = $category_controller->create($request);
+
+        $request->replace([
+            'category_id' => $cat_resto_1->id,
+            'name' => 'Sate',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est.',
+            'price' => 20000,
+            'unit' => 'qty',
+        ]);
+        $product_1_cat_resto_1 = $product_controller->create($request);
+
+        $request->replace([
+            'category_id' => $cat_resto_1->id,
+            'name' => 'Bakso',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est.',
+            'price' => 15000,
+            'unit' => 'qty',
+        ]);
+        $product_2_cat_resto_1 = $product_controller->create($request);
+
+        $request->replace([
+            'page_id' => $restoran->id,
+            'title' => 'Minuman',
+        ]);
+        $cat_resto_2 = $category_controller->create($request);
+
+        $request->replace([
+            'category_id' => $cat_resto_2->id,
+            'name' => 'Es Teh',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est.',
+            'price' => 3000,
+            'unit' => 'qty',
+        ]);
+        $product_1_cat_resto_2 = $product_controller->create($request);
+
+        $request->replace([
+            'category_id' => $cat_resto_2->id,
+            'name' => 'Susu putih',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In est.',
+            'price' => 5500,
+            'unit' => 'qty',
+        ]);
+        $product_2_cat_resto_2 = $product_controller->create($request);
+
+
+
+        // Page bengkel
+        $request->replace([
+            'title' => 'Bengkel Jaya Abadi',
+        ]);
+        $bengkel = $page_controller->create($request);
+
+        // Bengkel category
+        $request->replace([
+            'page_id' => $bengkel->id,
+            'title' => 'Servis',
+        ]);
+        $cat_bengkel_1 = $category_controller->create($request);
+
+        $request->replace([
+            'page_id' => $bengkel->id,
+            'title' => 'Lainnya',
+        ]);
+        $cat_bengkel_2 = $category_controller->create($request);
+
+
     }
 }
