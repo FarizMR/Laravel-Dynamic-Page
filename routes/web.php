cce55f4be3d19bf8165f3e6ea6d74f13f9320724
Fariz <frzrzky@gmail.com>
2021-10-11 00:55:33 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/landing',[App\Http\Controllers\PageController::class, 'showLandingPage']);
+Route::get('/admin',[App\Http\Controllers\AdminController::class, 'showAdminDashboard']);
+
+Route::group([
+    'prefix' => 'page',
+], function ($router) {
+    Route::get('/{slug}',[App\Http\Controllers\PageController::class, 'showServicePage']);
+    Route::post('/create',[App\Http\Controllers\PageController::class, 'create']);
+});
+
+Route::get('/api-test/page/{slug}',[App\Http\Controllers\PageController::class, 'show']);
